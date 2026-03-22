0a765765cb83d79f778416987150cdfba7c48112
Devel-Satanael <sdevlucascosta2017@gmail.com>
2026-03-22 15:21:40 -0300

    Adicionando todos os códigos de C, C# e Web da faculdade

---
+++ Terceiro Periodo/Professor Kaneko/Primeiro Bimestre/Ponto/Ponto.cs
@@ -0,0 +1,15 @@
+class Pessoa
+{
+    private strig nome[25];
+    private double idade;
+    private string sexo;
+
+    public()
+    {
+        idade = 0;
+        nome[25];
+        sexo;
+    }
+    public s()
+
+}
